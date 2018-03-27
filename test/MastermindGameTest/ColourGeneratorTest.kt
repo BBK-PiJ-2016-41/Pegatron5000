4e805c8056f2409c712778c40d2fe0c99e92c92f
Hannah <hannahlspencer@gmail.com>
2018-03-27 10:54:47 +0100

    Add test for randomness

--- test/MastermindGameTest/ColourGeneratorTest.kt
+++ test/MastermindGameTest/ColourGeneratorTest.kt
@@ -2,14 +2,14 @@ package MastermindGameTest
 
 import MastermindGame.ColourGenerator
 import MastermindGame.Colours.*
-import com.sun.org.apache.bcel.internal.util.BCELifier
 import org.junit.Before
 import org.junit.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertTrue
+import kotlin.test.assertNotEquals
 
 class ColourGeneratorTest {
     lateinit var availableColours : ArrayList<Colour>
+
     @Before
     fun initial() {
         val b = Bcolour()
@@ -35,6 +35,7 @@ class ColourGeneratorTest {
         val generator = ColourGenerator()
         val result = generator.generateSequence(4, availableColours)
         assertEquals(result.size, 4)
+
     }
 
     @Test
@@ -65,5 +66,21 @@ class ColourGeneratorTest {
         val generator = ColourGenerator()
         val result = generator.generateSequence(10, availableColours)
         assertEquals(result.size, 10)
+    }
+
+    @Test
+    fun testRandomness() {
+        val b = Bcolour()
+        val g = Gcolour()
+        val o = Ocolour()
+        val p = Pcolour()
+        val r = Rcolour()
+        val y = Ycolour()
+
+        val availableColours = arrayListOf<Colour>(b, g, o, p, r, y)
+        val generator = ColourGenerator()
+        val result = generator.generateSequence(5, availableColours)
+        val result2 = generator.generateSequence(5, availableColours)
+        assertNotEquals(result,result2)
     }
 }
