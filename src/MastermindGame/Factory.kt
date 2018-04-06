21026b2ecba11892f9fc6b118ce892044e5119b3
Hannah <hannahlspencer@gmail.com>
2018-04-06 19:33:50 +0100

    add test and file to find what colours are available to build with

--- src/MastermindGame/Factory.kt
+++ src/MastermindGame/Factory.kt
@@ -2,4 +2,24 @@ package MastermindGame
 
 object Factory {
     fun getInstance(c: Class<*>, b: Boolean?): Game = TODO()
+
+    fun findAvailableColours() : MutableList<Colour> {
+        var availableColours = mutableListOf<Colour>();
+        val colourLetters = arrayListOf<String>("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+                "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+
+        for(letter in colourLetters) {
+            try {
+                val classCalled = "MastermindGame.Colours." + letter + "colour"
+                val reflectClass = Class.forName(classCalled).kotlin.objectInstance as Colour
+                availableColours.add(reflectClass)
+            } catch(e: ClassNotFoundException) {
+                //if class is not found it means that there isn't a class of that name so skips over it
+            } catch(e: Exception) {
+                //no other exceptions expected, so alerts user if this is the case
+                print("An unexpected exception $e has been found")
+            }
+        }
+        return availableColours
+    }
 }
