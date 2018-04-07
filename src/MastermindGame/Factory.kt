1e0ace26f945c6199b3d6958b7278c0958476c8d
Kathryn Buckley <kathryn.buckley@live.co.uk>
2018-04-07 15:22:36 +0100

    Guess Validator now uses Hannah's implementation of Factory.findAvailableColours instead of enum

--- src/MastermindGame/Factory.kt
+++ src/MastermindGame/Factory.kt
@@ -2,4 +2,5 @@ package MastermindGame
 
 object Factory {
     fun getInstance(c: Class<*>, b: Boolean?): Game = TODO()
+    fun findAvailableColours() : MutableList<Colour> = TODO()
 }
--- src/MastermindGame/GuessValidator.kt
+++ src/MastermindGame/GuessValidator.kt
@@ -1,14 +1,8 @@
 package MastermindGame
 
 class GuessValidator(private val numPegs: Int) {
-    fun checkGuess(pegList: PegListGuessMock): Boolean {
-        return pegList.pegList.fold(0){sum, pegs ->
-            sum + if(coloursContains(pegs.colour)) 1 else 0} == numPegs
+    fun checkGuess(pegList: PegList): Boolean {
+        return pegList.getPegMap().map{peg -> peg.value}.fold(0){sum, pegs ->
+            sum + if(Factory.findAvailableColours().contains(pegs.colour)) 1 else 0} == numPegs
     }
-
-    private fun coloursContains(colour: String): Boolean {
-        ColoursMock.values().forEach{value -> if (value.name.equals(colour)) return true}
-        return false
-    }
-
 }
