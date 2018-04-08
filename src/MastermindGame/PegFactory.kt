afa1f7be75f97103244cde50091f8d8690b615d6
essvee <s.vincent@nhm.ac.uk>
2018-04-08 14:37:08 +0100

    Removes peg number validation

--- src/MastermindGame/PegFactory.kt
+++ src/MastermindGame/PegFactory.kt
@@ -12,13 +12,8 @@ object PegFactory {
 
     fun makePegs(input: MutableList<Colour>): PegList {
         val pegs = mutableListOf<Peg>()
+        input.forEach { pegs.add(PegImpl(it)) } // Turn each colour into a peg and add to list
 
-        // Check we've received the right number of pegs
-        if (input.size > listLength) {
-            print("Incorrect number of pegs!")
-        } else {
-            input.forEach { pegs.add(PegImpl(it)) } // Turn each colour into a peg and add to list
-        }
         return PegListImpl(pegs)
     }
 
