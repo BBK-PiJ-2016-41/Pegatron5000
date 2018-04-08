6799fe9c3e1e74577b39b60b54c3a36207e83762
essvee <s.vincent@nhm.ac.uk>
2018-04-08 19:26:03 +0100

    Changes white letter to 2 chars

--- src/MastermindGame/Colours/ResultColourWhite.kt
+++ src/MastermindGame/Colours/ResultColourWhite.kt
@@ -2,7 +2,7 @@ package MastermindGame.Colours
 
 object ResultColourWhite : Colour {
     override val name = "White"
-    override val letter = "W"
+    override val letter = "Wh"
 
     override fun toString(): String {
         return "$name "
