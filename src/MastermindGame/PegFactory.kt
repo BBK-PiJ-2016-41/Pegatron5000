4e37b04ae30670a11d60307f9567fc5f32ed79cd
Hannah <hannahlspencer@gmail.com>
2018-04-08 15:52:36 +0100

    Update working userInput method

--- src/MastermindGame/PegFactory.kt
+++ src/MastermindGame/PegFactory.kt
@@ -63,11 +63,11 @@ object PegFactory {
 
     //return mutable list of pegs
     fun interpretUserInput(userInput : String, numPegs: Int) : MutableList<Colour> {
-        val colours = findAvailableColours().map{colour -> Pair<colour.letter, colour>}
-        val result = userInput.split("").map{letter -> if(colours.containsKey(letter)) colours[colour.letter]
-            else throw InvalidArgumentException("Please enter a string with valid colours")}
-        if (result.length != numPegs) throw InvalidArgumentException("Please enter a string of length $numPegs")
-        else return result
+        val colours = findAvailableColours().map{colour -> Pair(colour.letter, colour)}.toMap()
+        val result = userInput.split("").filter{ letter -> (colours.containsKey(letter))}
+                .map{letter -> colours[letter] }.toMutableList()
+        if (result.size != numPegs || userInput.length != numPegs) throw InputMismatchException("Please enter a string of length $numPegs with the correct colours")
+        return result
     }
 
 }
