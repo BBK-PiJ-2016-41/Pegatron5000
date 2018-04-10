1779a21500eb2208b18e4df561640f9a1705b8f4
Hannah <hannahlspencer@gmail.com>
2018-04-10 19:45:17 +0100

    update doc

--- src/MastermindGame/PegFactory.kt
+++ src/MastermindGame/PegFactory.kt
@@ -20,7 +20,6 @@ object PegFactory {
     /**
      * method to generate sequence of colours
      * @param num : amount of colours needed, size of the sequence required
-     * @param availableColours : array list of all the colours available to be used for the sequence
      * @return randomColours : array list of the colours selected randomly
      */
     fun generateSequence(num : Int) : ArrayList<Colour> {
