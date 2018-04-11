92501bc6842da5f85c2bf767b7a5a22473632fa1
Hannah <hannahlspencer@gmail.com>
2018-04-11 09:10:10 +0100

    Add tests for exception in InterpretUserInput

--- test/MastermindGameTest/PegFactoryTest.kt
+++ test/MastermindGameTest/PegFactoryTest.kt
@@ -1,14 +1,14 @@
-//package MastermindGameTest
-//
-//import MastermindGame.*
-//import MastermindGame.Colours.*
-//import org.junit.Assert.*
-//import org.junit.Before
-//import org.junit.Test
-//
-//class PegFactoryTest {
-//    var listLength = 0
-//
+package MastermindGameTest
+
+import MastermindGame.*
+import MastermindGame.Colours.*
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import java.util.*
+
+class PegFactoryTest {
+    
 //    @Before
 //    fun build() {
 //        PegFactory.startFactory(4)
@@ -19,45 +19,55 @@
 //    fun testListLength() {
 //        assertEquals(4, PegFactory.listLength)
 //    }
-//
-//    @Test
-//    fun testFindingColours() {
-//        val fact = PegFactory
-//        val result = fact.findAvailableColours()
-//        val expected = mutableListOf<Colour>(Bcolour, Gcolour, Ocolour, Pcolour, Rcolour, Ycolour)
-//        assertEquals(expected, result)
-//    }
-//
-//    @Test
-//    fun testGenerate4Colours() {
-//        val result = PegFactory.generateSequence(4)
-//        assertEquals(result.size, 4)
-//    }
-//
-//    @Test
-//    fun testGenerate5Colours() {
-//        val result = PegFactory.generateSequence(5)
-//        assertEquals(result.size, 5)
-//    }
-//
-//    @Test
-//    fun testGenerate10Colours() {
-//        val result = PegFactory.generateSequence(10)
-//        assertEquals(result.size, 10)
-//    }
-//
-//    @Test
-//    fun testRandomness() {
-//        val result = PegFactory.generateSequence(4)
-//        val result2 = PegFactory.generateSequence(4)
-//        assertNotEquals(result,result2)
-//    }
-//
-//    @Test
-//    fun testInterpretUserInput() {
-//        val result = PegFactory.interpretUserInput("BGOP")
-//        val expected = mutableListOf<Peg>(PegImpl(Bcolour), PegImpl(Gcolour), PegImpl(Ocolour), PegImpl(Pcolour))
-//        assertEquals(expected.toString(), result.toString())
-//    }
-//
-//}
+
+    @Test
+    fun testFindingColours() {
+        val fact = PegFactory
+        val result = fact.findAvailableColours()
+        val expected = mutableListOf<Colour>(Bcolour, Gcolour, Ocolour, Pcolour, Rcolour, Ycolour)
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun testGenerate4Colours() {
+        val result = PegFactory.generateSequence(4)
+        assertEquals(result.size, 4)
+    }
+
+    @Test
+    fun testGenerate5Colours() {
+        val result = PegFactory.generateSequence(5)
+        assertEquals(result.size, 5)
+    }
+
+    @Test
+    fun testGenerate10Colours() {
+        val result = PegFactory.generateSequence(10)
+        assertEquals(result.size, 10)
+    }
+
+    @Test
+    fun testRandomness() {
+        val result = PegFactory.generateSequence(4)
+        val result2 = PegFactory.generateSequence(4)
+        assertNotEquals(result,result2)
+    }
+
+    @Test
+    fun testInterpretUserInput() {
+        val result = PegFactory.interpretUserInput("BGOP", 4)
+        val expected = mutableListOf<Peg>(PegImpl(Bcolour), PegImpl(Gcolour), PegImpl(Ocolour), PegImpl(Pcolour))
+        assertEquals(expected.toString(), result.toString())
+    }
+
+    @Test (expected = InputMismatchException::class)
+    fun testInterpretUserInputIncorrectLetter() {
+        val result = PegFactory.interpretUserInput("jfhf", 4)
+    }
+
+    @Test (expected = InputMismatchException::class)
+    fun testInterpretUserInputIncorrectLength() {
+        val result = PegFactory.interpretUserInput("BGOPBGOP", 4)
+    }
+
+}
